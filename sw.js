efc40cfa2b7bf5d0cc51a5492941f7a6d86ba116
정서영 <jsyoung@planica.co.kr>
2018-08-08 17:24:15 +0900

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-cbe41e775c26a816eb5b.js","75a83f273eba17c68b5f8fb90d6ae2bf"],["/commons-9316c45873f28938a9e1.js","13af3774497496e2344d32ed6d72569d"],["/index.html","80934d6b7311f7253883c259946d65f9"],["/manifest.webmanifest","70069d52a0a8f4a4c0d2ee79029a6a8c"],["/offline-plugin-app-shell-fallback/index.html","aabbe320bde909a31268448b40a56915"]];
+var precacheConfig = [["/app-cbe41e775c26a816eb5b.js","75a83f273eba17c68b5f8fb90d6ae2bf"],["/commons-9316c45873f28938a9e1.js","13af3774497496e2344d32ed6d72569d"],["/index.html","2b9a8f8c6ba18b69d65c50e4e51e91a6"],["/manifest.webmanifest","70069d52a0a8f4a4c0d2ee79029a6a8c"],["/offline-plugin-app-shell-fallback/index.html","bf243e0cb844e33eed10719b74d508f1"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
